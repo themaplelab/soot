a7ae77d06cdc263ba42759bbc494105f37661c07
knewbury <knewbury@ualberta.ca>
2019-06-19 15:50:13 +0000

    reworked the runall wrapper script, changes made: file exist checks for each step classpath fixes and phantom refs for soot cp file name handling read files to diff from the same config that gens them disable building in each run, waste

--- runCacheTest.sh
+++ runCacheTest.sh
@@ -3,40 +3,96 @@
 # src class gen jimple
 # vs
 # src cache gen jimple
+#
+# the class files are placed in:
+# the jimple from class src is also in:
+# /root/soot/cacheTestClassfiles
+#
+# the jimple from the cache src are in:
+# /root/soot/cacheTestCacheSrcJimple 
+#
+# ASSUMES test generators (/root/soot/src/main/java/soot/asm/tests/*.java)
+# are already built, wasteful to run it in here
 ##############
 
 #make the test generators
-./build.sh
+#TODO move these to a test compile, idk something wrong in maven phases right now probably not setup for custom jvm
+#./build.sh
 
-#go from gen->class and class->jimple
-./srctest.sh
+testconfig=./src/main/java/soot/asm/tests/tests.config
 
-#TODO add check that the prev steps generated the expected class and jimple files
+#try to go from gen->class and class->jimple
+./srctest.sh &> srctest.out
+cat srctest.out
+if ! cat srctest.out | grep -q "something went wrong"; then
+rm srctest.out
 
+ echo "########################################################"
+ echo "Generating classfiles and class->jimple step success!"
+ echo "########################################################"
+ 
+#use the config file for which tests were executed, to diff                                                         
+while IFS= read -r simpleTestname; do
+
+testname=${simpleTestname}Generated
+    
+    echo "########################################################"
+    echo "Running the cache src conversion: $simpleTestname"
+    echo "########################################################"
+    
 #make the cache->jimple
-#the cache placement run
+#the cache placement run, use same cache all tests, why not
 java -Xshareclasses:name=cacheSrcTest  \
      -Djava.library.path=$LD_LIBRARY_PATH:/root/openj9-openjdk-jdk8/openj9/runtime/ddrext:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/jdk/classes/com/ibm/oti/shared/ \
-     -cp /root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/jdk/classes/com/ibm/oti/shared/:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/images/j2sdk-image/jre/lib/rt.jar:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/images/j2sdk-image/jre/lib/ddr/j9ddr.jar:/root/soot/cacheTest/:target/sootclasses-trunk-jar-with-dependencies.jar:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/jdk/ddr/classes/com/ibm/j9ddr/vm29/structure/ cacheSrcTest
+     -cp /root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/jdk/classes/com/ibm/oti/shared/:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/images/j2sdk-image/jre/lib/rt.jar:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/images/j2sdk-image/jre/lib/ddr/j9ddr.jar:/root/soot/cacheTestClassfiles/:target/sootclasses-trunk-jar-with-dependencies.jar:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/jdk/ddr/classes/com/ibm/j9ddr/vm29/structure/ $testname &> /dev/null
 
-#the actual run
+#the actual translation run
 java  -Dcom.ibm.j9ddr.structurefile=/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/vm/j9ddr.dat \
       -Xshareclasses:name=cacheSrcTest \
       -Djava.library.path=$LD_LIBRARY_PATH:/root/openj9-openjdk-jdk8/openj9/runtime/ddrext:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/jdk/classes/com/ibm/oti/shared/\
-      -cp /root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/jdk/classes/com/ibm/oti/shared/:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/images/j2sdk-image/jre/lib/rt.jar:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/images/j2sdk-image/jre/lib/ddr/j9ddr.jar:/root/soot/tests/:target/sootclasses-trunk-jar-with-dependencies.jar:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/jdk/ddr/classes/com/ibm/j9ddr/vm29/structure/ \
+      -cp /root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/jdk/classes/com/ibm/oti/shared/:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/images/j2sdk-image/jre/lib/rt.jar:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/images/j2sdk-image/jre/lib/ddr/j9ddr.jar:/root/soot/cacheTestClassfiles/:target/sootclasses-trunk-jar-with-dependencies.jar:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/jdk/ddr/classes/com/ibm/j9ddr/vm29/structure/ \
       -XshowSettings -Xmx5g soot.Main \
-      -f J -src-prec cache cacheSrcTest
+      -cp /root/soot/cacheTestClassfiles:/root/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/images/j2sdk-image/jre/lib/rt.jar \
+      -f J -src-prec cache -d cacheTestCacheSrcJimple \
+      -allow-phantom-refs \
+      $testname
 
-#TODO replace this to read from some config file
-if [ ! -f ./sootOutput/cacheSrcTest.jimple ];then
-    echo "test failed"
+#first lets check if prev steps made the jimples
+jimple=$testname.jimple
+
+if [ ! -f ./cacheTestCacheSrcJimple/$jimple ]; then
+    echo "########################################################"
+    echo "Test failed at cache->jimple:"
+    echo "jimple file $cachejimple not generated"
+    echo "########################################################"
+
+elif [ ! -f ./cacheTestClassfiles/$jimple ]; then
+    echo "########################################################"
+    echo "Test failed at class->jimple:"
+    echo "jimple file $classjimple not generated"
+    echo "########################################################"
+	
+else
+
+    #files exist
+    if ! diff -q  ./cacheTestClassfiles/$jimple ./cacheTestCacheSrcJimple/$jimple &>/dev/null; then
+	echo "########################################################"
+	echo "Test failed: ./cacheTestClassfiles/$jimple and ./cacheTestCacheSrcJimple/$jimple contain differences."
+	echo "########################################################"
+	diff -y --left-column ./cacheTestClassfiles/$jimple ./cacheTestCacheSrcJimple/$jimple >> diffs/$simpleTestname.txt
+    else
+	echo "########################################################"
+	echo "Test passed: ./cacheTestClassfiles/$jimple and ./cacheTestCacheSrcJimple/$jimple are same."
+	echo "########################################################"
+    fi
+    
+     fi
+done<$testconfig
 
 else
 
-    if ! diff -q sootOutput/cacheSrcTest.jimple AllOpsTest/cacheSrcTest.jimple &>/dev/null; then
-	diff -y --left-column sootOutput/cacheSrcTest.jimple AllOpsTest/cacheSrcTest.jimple
-    else
-	echo "test passed"
-    fi
+    echo "########################################################"
+    echo "Generating classfiles and class->jimple step failed"
+    echo "########################################################"
     
 fi
