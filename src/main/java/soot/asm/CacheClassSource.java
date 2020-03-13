41e06a0b83975d119c6cfb56879f7f7af99ebcb6
Kristen Newbury <knewbury@ualberta.ca>
2020-03-13 16:53:11 +0000

    .

--- src/main/java/soot/asm/CacheClassSource.java
+++ src/main/java/soot/asm/CacheClassSource.java
@@ -4,7 +4,7 @@ package soot.asm;
  * #%L
  * Soot - a J*va Optimization Framework
  * %%
- * Copyright (C) 1997 - 2019 Kristen Newbury
+ * Copyright (C) 1997 - 2014 Raja Vallee-Rai and others 
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
--- src/main/java/soot/asm/CacheMemorySingleton.java
+++ src/main/java/soot/asm/CacheMemorySingleton.java
@@ -4,7 +4,7 @@ package soot.asm;
  * #%L                                                                                                                                   
  * Soot - a J*va Optimization Framework                                                                                                  
  * %%                                                                                                                                    
- * Copyright (C) 2019 Kristen Newbury                                                                                                    
+ * Copyright (C) 1997 - 2014 Raja Vallee-Rai and others        
  * %%                                                                                                                                    
  * This program is free software: you can redistribute it and/or modify                                                                  
  * it under the terms of the GNU Lesser General Public License as                                                                        
@@ -33,6 +33,7 @@ import java.lang.NoSuchFieldException;
 import com.ibm.j9ddr.corereaders.memory.BufferedMemory;
 import com.ibm.j9ddr.corereaders.memory.BufferedMemorySource;
 
+//@author Kristen Newbury
 public class CacheMemorySingleton{
 
     //this singleton can only have one memory source ever added to it
